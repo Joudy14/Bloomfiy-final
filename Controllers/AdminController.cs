f8f630b09255cb54a75ff74130ec28fcde000c06
Gaod Nabıl Ahmed Al Zubaır <2300001068@stu.iku.edu.tr>
2025-12-08 22:08:21 +0300

    ok

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Bloomfiy_final.Controllers.Admin
+namespace Bloomfiy_final.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
-        // GET: Admin
-        public ActionResult Index()
+        // GET: /Admin/Dashboard
+        public ActionResult Dashboard()
         {
             return View();
         }
