26d42ec9d298fa002402cac5b9e797d20c2f194b
Gaod Nabıl Ahmed Al Zubaır <2300001068@stu.iku.edu.tr>
2025-12-12 17:51:26 +0300

    i think i'm prone to getting blinded when it's bright

---
+++ Models/ChexkoutInputModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bloomfiy_final.Models
+{
+    public class CheckoutInputModel
+    {
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        // Payment (fake)
+        [Required]
+        [CreditCard]
+        public string CardNumber { get; set; }
+
+        [Required]
+        public string Expiry { get; set; }
+
+        [Required]
+        public string CVV { get; set; }
+    }
+}
